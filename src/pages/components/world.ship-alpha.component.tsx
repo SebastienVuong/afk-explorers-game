9fefb8a22c3e53ccb6a8c7ccd014cc125c8af51b
Sébastien Vuong <sebastien.vuong.teamcanada@gmail.com>
2023-05-17 00:39:45 -0400

    refactor: pull afk earnings into custom hook

--- src/pages/components/world.ship-alpha.component.tsx
+++ src/pages/components/world.ship-alpha.component.tsx
@@ -1,38 +1,18 @@
-import { useAtomValue, useSetAtom } from "jotai";
+import { useAtomValue } from "jotai";
 import { heroesAtom } from "../../atoms/heroes.atom";
 import { HeroCard } from "../../shared/hero-card.component";
 import { shipAlphaAtom } from "../../atoms/ship.atom";
 import { EmptyShipBody } from "./world.empty-ship.component";
 import { Title } from "../../shared/title.component";
-import { useEffect, useState } from "react";
-import { goldAtom } from "../../atoms/gold.atom";
+import { useAFKHeroEarnings } from "../../hooks/use-afk-hero-earnings.hook";
 
-const HP_COST_PER_SECOND = 2;
+export const HP_COST_PER_SECOND = 2;
 
 export const ShipAlpha = () => {
   const ship = useAtomValue(shipAlphaAtom);
   const leadingHero = useAtomValue(heroesAtom).find(hero => hero.id === ship.leadingHeroId);
-  const setGold = useSetAtom(goldAtom)
-  const [timeCounter, setTimeCounter] = useState(0)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setTimeCounter(timeCounter + 1)
-    }, 1000)
-  }, [timeCounter])
-
-  useEffect(() => {
-    if (!leadingHero) return
-    const isHeroActive = leadingHero.hp > 0
-    if (!isHeroActive) return
-    const heroHpAfterCost = leadingHero.hp - HP_COST_PER_SECOND
-    if (heroHpAfterCost < 0) {
-      leadingHero.hp = 0
-    } else {
-      leadingHero.hp = heroHpAfterCost
-      setGold(gold => gold + leadingHero.earnings)
-    }
-  }, [timeCounter])
+  useAFKHeroEarnings(leadingHero)
 
   return (
     <div>
